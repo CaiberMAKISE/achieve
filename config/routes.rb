812e8689c4817e2a5c9a364e180243e3c42209de
Naoya Fujio <naofudiwo@gmail.com>
2020-08-20 15:44:52 +0000

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs
+  get '/route', to: 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
